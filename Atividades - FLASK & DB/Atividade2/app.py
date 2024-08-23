983061cfe5dcd0edbd0a0d5a5ecaa3e9d1bb688b
JulioTVRS <juliotvrs777@gmail.com>
2024-08-23 17:30:46 -0300

    commit

--- Atividades - FLASK & DB/Atividade2/app.py
+++ Atividades - FLASK & DB/Atividade2/app.py
@@ -1,64 +1,70 @@
-from flask import Flask, request, render_template, url_for, \
-    session, redirect
+from flask import Flask, request, render_template, url_for, redirect
+import sqlite3
 
 app = Flask(__name__)
 
-bancodados = {}
 
-app.config['SECRET_KEY'] = 'superdificil'
+def get_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/dashboard')
-def dash():    
-    if 'user' in session:
-        return render_template('dashboard.html')
-    else:
-        return redirect(url_for('register'))
+def dash():
+    nome = request.args.get('nome')
+    senha = request.args.get('senha')
+
+    if nome and senha:
+        conn = get_connection()
+        user = conn.execute('SELECT * FROM users WHERE nome = ? and senha = ?', (nome, senha)).fetchone()
+        conn.close()
+
+        if user:
+            return render_template('dashboard.html', nome=nome)
+    
+    
+    return render_template('login.html')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-
-    if 'user' in session:
-        return redirect(url_for('dash'))
-
     if request.method == 'POST':
         nome = request.form['nome']
         senha = request.form['senha']
 
-        if nome in bancodados and bancodados[nome] == senha:
-            session['user'] = nome
-            return redirect(url_for('dash'))
-        else:
-            return "usuario inexistente ou senha incorreta"
+        conn = get_connection()
+        user = conn.execute('SELECT * FROM users WHERE nome = ? and senha = ?', (nome, senha)).fetchone()
+        conn.close()
 
+        if user:
+            return redirect(url_for('dash', nome=nome, senha=senha))
+        else:
+            return "Usuário não existe ou a senha está incorreta."
 
     return render_template('login.html')
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-
-    ## esse bloco tanto para register quanto para login
-    if 'user' in session:
-        return redirect(url_for('dash'))
-
     if request.method == 'POST':
         nome = request.form['nome']
         senha = request.form['senha']
 
-        # regsitrar no banco 
-        if nome not in bancodados:
-            bancodados[nome] = senha
-            session['user'] = nome
-            return redirect(url_for('dash'))
+        conn = get_connection()
+        user = conn.execute('SELECT * FROM users WHERE nome = ?',(nome,)).fetchone()
+
+        if user:
+            return "Usuário já cadastrado"
         else:
-            return "Ja estas cadastrado"
+            conn.execute('INSERT INTO users (nome, senha) VALUES (?, ?)', (nome, senha))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('dash', nome=nome, senha=senha))
 
     return render_template('register.html')
 
 @app.route('/logout', methods=['POST', 'GET'])
 def logout():
-    session.pop('user', None)
-    return redirect(url_for('index'))
+    return render_template('index.html')
