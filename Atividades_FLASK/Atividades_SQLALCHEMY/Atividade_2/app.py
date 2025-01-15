f130eda2d9be20bc8998922b1aea5e46f91ef44a
JulioTVRS <151770679+JulioTVRS@users.noreply.github.com>
2025-01-15 17:51:53 -0300

    add

--- Atividades_FLASK/Atividades_SQLALCHEMY/Atividade_2/app.py
+++ Atividades_FLASK/Atividades_SQLALCHEMY/Atividade_2/app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    nome: Mapped[str] = mapped_column(nullable=False)
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projeto.db'
@@ -18,4 +23,15 @@ with app.app_context():
 
 @app.route('/')
 def index():
+    user = User(nome="Pessoa")
+
+    db.session.add(user)
+    db.session.commit()
+    db.session.close()
+
     return render_template('index.html')
+
+@app.route('/listar')
+def listar():
+    resultado = db.session.query(User).all()
+    return render_template('listar.html', resultado=resultado)
